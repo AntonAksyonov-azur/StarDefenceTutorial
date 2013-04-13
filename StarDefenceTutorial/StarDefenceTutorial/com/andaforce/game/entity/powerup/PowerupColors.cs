d14f171dc5278a76aa9e02303d7b16e8d4ff1778
za3a24 <za3a24@yandex.ru>
2013-04-13 21:11:33 +0700

    Added dictionary that links color with powerups

---
+++ StarDefenceTutorial/StarDefenceTutorial/com/andaforce/game/entity/powerup/PowerupColors.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace StarDefenceTutorial.com.andaforce.game.entity.powerup
+{
+    public class PowerupColors
+    {
+        private static PowerupColors _instance;
+        public Dictionary<PowerupType, Color> Colors = new Dictionary<PowerupType, Color>();
+
+        private PowerupColors()
+        {
+            Colors.Add(PowerupType.AdditionalLives, Color.Red);
+            Colors.Add(PowerupType.AdditionalScores, Color.Green);
+            Colors.Add(PowerupType.IncreasedFirerate, Color.Yellow);
+            Colors.Add(PowerupType.IncreasedFirerate, Color.Violet);
+            Colors.Add(PowerupType.IncreasedVelocity, Color.Blue);
+            Colors.Add(PowerupType.IncreatedWeapon, Color.Orange);
+        }
+
+        public static PowerupColors GetInstance()
+        {
+            return _instance ?? (_instance = new PowerupColors());
+        }
+    }
+}
